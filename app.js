973c40830cfe58d88c647adc75986034d368ed81
Giuseppe Augello <fullstack.development.aug@gmail.com>
2018-11-17 19:23:30 +0000

    Form submit + html response server lesson commit

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const http = require('http');
 // }
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-   // process.exit(); this is to leave the process
-
+    const url = req.url;
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type ="text"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
    res.setHeader('Content-Type', 'text/html');
    res.write('<html>');
    res.write('<head><title>My First Page</title></head>');
@@ -17,5 +22,9 @@ const server = http.createServer((req, res) => {
    res.write('</html>');
    res.end();//res.end is used to end  the response
 });
+
+
+
+
 
 server.listen(3000);
