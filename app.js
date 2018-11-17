f46ff69265b3eba079643fbde1d70db9fe97e9fa
Giuseppe Augello <fullstack.development.aug@gmail.com>
2018-11-17 19:56:11 +0000

    Sezione 3 lezione 33

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 /*This how you write the server on node.js,*/ 
 
 const http = require('http');
-
+const fs = require('fs');
 // function rqListener(req, res){
 
 // }
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if(url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type ="text"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end(); 
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
+
    res.setHeader('Content-Type', 'text/html');
    res.write('<html>');
    res.write('<head><title>My First Page</title></head>');
